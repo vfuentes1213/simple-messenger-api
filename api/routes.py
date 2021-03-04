1a73a16b7b7cb69f99577898bec48613f5ac82f2
Victor Fuentes <vfuentes1213@gmail.com>
2021-03-04 08:43:09 -0700

    added user routes

--- api/routes.py
+++ api/routes.py
@@ -1,5 +1,9 @@
-from api.resources.message import MessageList, RecipientMessages
+from resources.message import MessageList, RecipientMessages
+from resources.user import UserList, User
+
 
 def initialize_routes(api):
     api.add_resource(RecipientMessages, "/message/<int:recipient_id>")
     api.add_resource(MessageList, "/messages")
+    api.add_resource(User, "/user")
+    api.add_resource(UserList, "/users")
