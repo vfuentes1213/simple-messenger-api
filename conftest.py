3e4bcf628076c2ac05e40a5be489056a9ecd6dfa
Victor Fuentes <vfuentes1213@gmail.com>
2021-03-04 20:55:42 -0700

    added global fixture for db connection

--- conftest.py
+++ conftest.py
@@ -1,15 +1,20 @@
 import pytest
 from api.main import app
 
+
 @pytest.fixture
 def client():
     app.config["TESTING"] = True
     client = app.test_client()
     yield client
 
+
 @pytest.fixture(autouse=True)
 def mock_db_connection(mocker):
-    mocker.patch('api.db.db')
+    mocker.patch("api.db.db")
+
 
 def pytest_configure(config):
-    config.addinivalue_line("markers", "integration: services are not mocked")
+    config.addinivalue_line(
+        "markers", "integration: services,connections,etc are not mocked"
+    )
